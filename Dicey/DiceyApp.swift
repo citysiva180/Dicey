3b4f3d3fa37948c3264d93a1e0474cd220d6b818
shivthedev <citysiva180@users.noreply.github.com>
2024-02-29 05:46:36 +0530

    Initial Commit

---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Sivarajan on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
